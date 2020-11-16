776e26de6ae938a5b84f049291f82e886a7fdfce
Rifat Ahmed Habibullah <habibullahrifat@outlook.com>
2020-11-16 12:43:48 +0200

    Update my_program.py
    Review

--- my_program.py
+++ my_program.py
@@ -1,71 +1,85 @@
 {
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Enter your name Micheal\n"
-     ]
-    }
+   "cells":[
+      {
+         "cell_type":"code",
+         "execution_count":4,
+         "metadata":{
+            
+         },
+         "outputs":[
+            {
+               "name":"stdout",
+               "output_type":"stream",
+               "text":[
+                  "Enter your name Micheal\n"
+               ]
+            }
+         ],
+         "source":[
+            "name = input(\"Enter your name here \")"
+         ]
+      },
+      {
+         "cell_type":"code",
+         "execution_count":5,
+         "metadata":{
+            
+         },
+         "outputs":[
+            {
+               "name":"stdout",
+               "output_type":"stream",
+               "text":[
+                  "Hello   Micheal\n"
+               ]
+            }
+         ],
+         "source":[
+            "print (\"Hello  \", name) "
+         ]
+      },
+      {
+         "cell_type":"code",
+         "execution_count":6,
+         "metadata":{
+            
+         },
+         "outputs":[
+            {
+               "name":"stdout",
+               "output_type":"stream",
+               "text":[
+                  "You can now do the review\n"
+               ]
+            }
+         ],
+         "source":[
+            "print (\"You can now do the review\")"
+         ]
+      },
+      {
+         "cell_type":"code",
+         "execution_count":null,
+         "metadata":{
+            
+         },
+         "outputs":[
+            
+         ],
+         "source":[
+            
+         ]
+      }
    ],
-   "source": [
-    "name = input(\"Enter your name here \")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 5,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Hello   Micheal\n"
-     ]
-    }
-   ],
-   "source": [
-    "print (\"Hello  \", name) "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 6,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "You can now do the review\n"
-     ]
-    }
-   ],
-   "source": [
-    "print (\"You can now do the review\")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
+   "metadata":{
+      "kernelspec":{
+         "display_name":"Python 3",
+         "language":"python",
+         "name":"python3"
+      }
+   },
+   "nbformat":4,
+   "nbformat_minor":2
 }
+# json object with array
+# cells with variant data
